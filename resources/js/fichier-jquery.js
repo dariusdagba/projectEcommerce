9ff1f8fc5a98036dfe8847a5ac3772579ac9d624
dariusdagba <dariogbadoo@gmail.com>
2025-03-13 23:26:12 -0400

    first commit

---
+++ resources/js/fichier-jquery.js
@@ -0,0 +1,18 @@
+let promo=document.querySelector("#store-promo");
+
+// $(document).ready(function() {
+//     function checkWidth() {
+//         let windowWidth = $(window).width();
+//         if (windowWidth < 992) { // Correspond à 'md' et 'sm' en Bootstrap
+//             $('#store-promo').hide();
+//         } else {
+//             $('#store-promo').show();
+//         }
+//     }
+
+//     // Exécuter au chargement de la page
+//     checkWidth();
+
+//     // Exécuter à chaque redimensionnement de la fenêtre
+//     $(window).resize(checkWidth);
+// });
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/assets/icone.png">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Avery Shop - Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div id="store-promo" class="container-fluid d-lg-flex justify-content-between d-none d-lg-block">
+        <img src="/assets/icone.png" class="mt-1" width="20" height="20" alt="Avery Shop Icon">
+        <ul class="nav justify-content-end ">
+            <li class="nav-item"><a class="nav-link text-black" href="#find-store">Find a store |</a></li>
+            <li class="nav-item"><a class="nav-link text-black" href="#promotions">View promotions |</a></li>
+            <li class="nav-item"><a class="nav-link text-black" href="#help">Help</a></li>
+        </ul>
+</div>
+    <header class="">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-light bg-light">
+
+            <div class="container-fluid">
+                <img src="/assets/icone.png" alt="icone" width="30" height="30" class="d-inline-block align-text-top me-5">
+              <a class="navbar-brand" href="#">Avery Shop</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item me-5">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li class="nav-item me-5">
+                    <a class="nav-link" href="/contact">Contact</a>
+                  </li>
+                  <li class="nav-item me-5">
+                    <a class="nav-link" href="/join">Join Us</a>
+                  </li>
+                  <li class="nav-item me-5">
+                    <a class="nav-link" href="/about">About</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Connexion
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Sign In</a></li>
+                      <li><a class="dropdown-item" href="#">Sign Up</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                    <div class="input-group input-group-append">
+                        <input class="form-control me-2 rounded-start-5" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success rounded-end-5" type="submit">Search</button>
+                    </div>
+
+                  </form>
+              </div>
+
+            </div>
+          </nav>
+    </header>
+    <section class="mb-4">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators" >
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="10000">
+                <div class="row row-cols-lg-2 row-cols-1">
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h.webp" class="d-block w-100" alt="First Image">
+                    </div>
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h2.webp" class="d-block w-100" alt="Second Image">
+                    </div>
+                </div>
+                <div class="carousel-caption d-none d-md-block">
+                  <h1>ZOOM VOMERO</h1>
+                  <p>Full-length dynamic shoe. Get sensation in every step!</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <div class="row row-cols-lg-2 row-cols-1">
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h3.webp" class="d-block w-100" alt="Third Image">
+                    </div>
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h4.webp" class="d-block w-100" alt="Fourth Image">
+                    </div>
+                </div>
+                <div class="carousel-caption d-none d-md-block">
+                  <h1>ZOOM VOMERO</h1>
+                  <p>Full-length dynamic shoe. Get sensation in every step!</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <div class="row row-cols-lg-2 row-cols-1">
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h5.jpg" class="d-block w-100 h-100" alt="Fifth Image">
+                    </div>
+                    <div class=" p-0">
+                        <img src="/assets/nike-v-h6.jpg" class="d-block w-100 h-100" alt="Sixth Image">
+                    </div>
+                </div>
+                <div class="carousel-caption d-none d-md-block">
+                  <h1>ZOOM VOMERO</h1>
+                  <p>Full-length dynamic shoe. Get sensation in every step!</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </section>
+    <section class="mb-3">
+        <div class="container">
+            <div class="row row-cols-lg-3 row-cols-1 row-cols-md-2">
+                <div class="col position-relative">
+                    <img src="/assets/modele-1.png" class="d-block w-100" alt="First Image">
+                    <button class="btn btn-outline-success position-absolute bottom-0 end-0 m-3 rounded-end-4 rounded-start-4">Women</button>
+                </div>
+                <div class="col position-relative">
+                    <img src="/assets/modele-2.jpg" class="d-block w-100" alt="Second Image">
+                    <button class="btn btn-outline-success position-absolute bottom-0 end-0 m-3 rounded-end-4 rounded-start-4">Men</button>
+                </div>
+                <div class="col position-relative">
+                    <img src="/assets/modele-3.jpg" class="w-100 h-100" alt="Third Image">
+                    <button class="btn btn-outline-success position-absolute bottom-0 end-0 m-3 rounded-end-4 rounded-start-4">Kids</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mb-3">
+        <div class="container">
+            <div class="row row-cols-lg-2 row-cols-1">
+                <div class="col position-relative">
+                    <img src="/assets/modele-4.jpg" class="w-100 h-100" alt="Fourth Image">
+                    <div class="position-absolute bottom-0 start-0 m-3">
+                        <h2>New in football</h2>
+                        <p>Mad Energy Pack</p>
+                        <button class="btn btn-outline-success rounded-end-5 rounded-start-4">Shop Now</button>
+                    </div>
+                </div>
+                <div class="col position-relative">
+                    <img src="/assets/modele-5.jpg" class="w-100 h-100" alt="Fourth Image">
+                    <div class="position-absolute bottom-0 start-0 m-3">
+                        <h2>New in Running</h2>
+                        <p>Vomero 18.</p>
+                        <button class="btn btn-outline-success rounded-end-4 rounded-start-4">Shop Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mb-3"></section>
+    <section class="mb-3"></section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
